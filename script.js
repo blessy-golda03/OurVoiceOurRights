b9b19435b99745c0b9887894e956b5ef5157956c
blessy-golda03 <blessyarul03@gmail.com>
2025-11-01 10:36:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.getElementById("feedbackForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  const name = document.getElementById("name").value;
-  const district = document.getElementById("district").value;
-  const feedback = document.getElementById("feedback").value;
 
-  document.getElementById("responseMsg").innerText = 
-    `🙏 Thank you, ${name} from ${district}! Your feedback has been recorded.`;
-  
-  this.reset();
-});
